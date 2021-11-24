7712fee78b104ae0913e6cc896c9271b64bf777a
Stephanie <stephogam@gmail.com>
2021-11-24 23:32:49 +0000

    Fix server localhost; error persists

--- server.py
+++ server.py
@@ -4,5 +4,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def show_landing_page():
+  "Display landing page."
+
+  return render_template("base.html")
+
 
 if __name__ == '__main__':
+  app.run(debug=True, host="0.0.0.0", port="5000")
