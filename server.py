d2248f5675f25334fff5efe16c7e2e0e5279b5fc
Stephanie <stephogam@gmail.com>
2021-11-26 23:11:07 +0000

    App not loading properly; fix connection

--- server.py
+++ server.py
@@ -16,5 +16,16 @@ def show_landing_page():
   return render_template("base.html")
 
 
+# if __name__ == '__main__':
+#   app.run(debug=True, host="0.0.0.0", port="5000")
+
 if __name__ == '__main__':
-  app.run(debug=True, host="0.0.0.0", port="5000")
+    import sys, os
+    if len(sys.argv) > 1:
+        # arg, if any, is the desired port number
+        port = int(sys.argv[1])
+        assert(port>1024)
+    else:
+        port = os.getuid()
+    app.debug = True
+    app.run('0.0.0.0', port="8080")
